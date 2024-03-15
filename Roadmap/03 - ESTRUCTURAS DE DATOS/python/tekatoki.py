0dce4f09ae2a7b7575187734af251da8c8316ccc
tekatoki <danoid2007@gmail.com>
2024-03-15 23:42:49 +0100

    Add problem statement

---
+++ Roadmap/03 - ESTRUCTURAS DE DATOS/python/tekatoki.py
@@ -0,0 +1,23 @@
+'''
+╔═══════════════════════════════════════╗
+║ Autor:  tekatoki                      ║
+║ GitHub: https://github.com/tekatoki   ║
+║ 2024 -  Python                        ║
+╚═══════════════════════════════════════╝
+
+Exercise:
+- Show examples of the creation of all structures supported
+by default in your language.
+- Use operation of insertions, earase, update, sort.
+
+Difficulty extra (optional):
+Create a contacts agenda on the terminal
+- Must implement fucnctionality of search, insection, update and 
+delete contacts
+- Each contact must have a name and a telephone number
+- The pogram ask the first place what is the operation to be done and then
+ask the necessary data
+- The program mustn't let the user introduce telephone numbers no numeric or with
+more than 11 digits. 
+- You must put an operation that finish the program
+'''
