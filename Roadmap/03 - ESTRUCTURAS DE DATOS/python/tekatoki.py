de1ce58e9a07ba1aee186901325900ace20169ad
tekatoki <danoid2007@gmail.com>
2024-04-04 22:56:22 +0200

    #03 Python: Finish first part of exercise

--- Roadmap/03 - ESTRUCTURAS DE DATOS/python/tekatoki.py
+++ Roadmap/03 - ESTRUCTURAS DE DATOS/python/tekatoki.py
@@ -23,14 +23,44 @@ more than 11 digits.
 - You must put an operation that finish the program
 '''
 
-
 def simple_exercise()->None:
     ex_list:list[str] = ['h', '2', 'hi']
-    ex_tuple:tuple[int] = [23, 51, 24]
+    ex_tuple:tuple[int] = (23, 51, 24)
     ex_set:set[float] = {73.0, 3.4, 3.1415}
     ex_dict:dict = {'name': 'Jack', 'lastname': 'Bluepark', 'age': 21}
 
-def chose_function(function_1:function, function_2:function, key_function:int=0)-> None:
+    print(ex_list, ex_tuple, ex_set, ex_dict)
+
+    # List's methods
+    ex_list.append('ok')
+    print(f'Append method [list] > {ex_list}')
+
+    ex_list.extend(['nothing', 'xz', 'mathematics are sexy'])
+    print(f'Extend method [list] > {ex_list}')
+
+    ex_list.insert(3, 'maboy')
+    print(f'Insert method [list] > {ex_list}')
+
+    ex_list.remove('2')
+    print(f'Remove method [list] > {ex_list}')
+    
+    ex_list.pop(2)
+    print(f'Pop method [list] > {ex_list}')
+
+    ex_list.clear()
+    print(f'Clear method [list] > {ex_list}')
+
+    ex_list.extend(['nothing', 'xz', 'mathematics are sexy'])
+    print(f'Added things to clear list > {ex_list}')
+
+    ex_list.sort()
+    print(f'Sort method [list]> {ex_list}')
+
+    ex_list.reverse()
+    print(f'Reverse method [list] > {ex_list}')
+    
+
+def chose_function(function_1, function_2, key_function:int=0)-> None:
     if 0 == key_function:
         function_1()
     elif 1 == key_function:
@@ -40,7 +70,8 @@ def chose_function(function_1:function, function_2:function, key_function:int=0)
 
 
 def main()->None:
-    print()
+    in_user:int = int(input('Print [0, 1] >'))
+    chose_function(simple_exercise, None, in_user)
 
 if __name__ == '__main__':
     main()
